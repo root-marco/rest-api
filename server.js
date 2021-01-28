ae6c5b861f9503a172b1fb81d2bf1edb33e95be8
chaos <rootslowed@gmail.com>
2021-01-27 21:07:34 -0300

    modules, mongoose and listen done

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+// MODULES
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+// MONGOOSE
+mongoose.connect(process.env.DATABASE_URL, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('database connected'));
+
+// LISTEN
 app.listen(PORT, () => console.log('server running'));
