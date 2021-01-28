953d64e4001faab302291e3281b460f0cc9ec6b9
chaos <rootslowed@gmail.com>
2021-01-28 12:48:05 -0300

    subscriber schema created with name, subscriberToChannel and subscriberDate

---
+++ models/subscriber.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const subscriberSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required,
+	},
+	subscriberToChannel: {
+		type: String,
+		required,
+	},
+	subscriberDate: {
+		type: Date,
+		required: true,
+		default: Date.now(),
+	},
+});
