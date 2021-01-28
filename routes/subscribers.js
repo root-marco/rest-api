f888b17b9c06818bd51a2e195354005db8aa453f
chaos <rootslowed@gmail.com>
2021-01-28 12:58:37 -0300

    root router created with async/await

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,20 +1,26 @@
 import express from 'express';
+import Subscriber from '../models/subscriber.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('<h1>HELLO</h1>');
+router.get('/', async (req, res) => {
+	try {
+		const subscribers = await Subscriber.find();
+		res.json(subscribers);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 router.get('/:id', (req, res) => {
 	res.send(req.params.id);
 });
 
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
 	
 });
 
-router.post('/', (req, res) => {
+router.patch('/', (req, res) => {
 	
 });
 
