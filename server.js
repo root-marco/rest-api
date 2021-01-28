e0f28e6d7ded57c78bf2e9df7e037ef4f5201e56
chaos <rootslowed@gmail.com>
2021-01-27 21:25:23 -0300

    subscribersRouter in app use

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// MODULES
+import subscribersRouter from './routes/subscribers.js';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -7,18 +7,16 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-
-// MONGOOSE
 mongoose.connect(process.env.DATABASE_URL, {
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 });
-
 const db = mongoose.connection;
-
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('database connected'));
 
-// LISTEN
+app.use('subscribers', subscribersRouter);
+
+app.use(express.json());
+
 app.listen(PORT, () => console.log('server running'));
