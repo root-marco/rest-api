3b47485ec9095eb94a0a147dd65e93c0665bad70
chaos <rootslowed@gmail.com>
2021-02-01 13:16:26 -0300

    logic to create a new subscriber implemented

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -4,28 +4,48 @@ import Subscriber from '../models/subscriber.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+
 	try {
 		const subscribers = await Subscriber.find();
 		res.json(subscribers);
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		res.status(500).json({
+			message: err.message,
+		});
 	}
+
 });
 
 router.get('/:id', (req, res) => {
+
 	res.send(req.params.id);
+
 });
 
-router.post('/',  (req, res) => {
-	
-});
+router.post('/', async (req, res) => {
+
+	const subscriber = new Subscriber({
+		name: req.body.name,
+		subscribedToChannel: req.body.subscribedToChannel,
+	});
+
+	try {
+		const newSubscriber = await subscriber.save();
+		res.status(201).json(newSubscriber);
+	} catch (err) {
+		res.status(400).json({
+			message: err.message,
+		});
+	}
+
+})
 
 router.patch('/', (req, res) => {
-	
+
 });
 
 router.delete('/:id', (req, res) => {
-	
+
 });
 
 export default router;
